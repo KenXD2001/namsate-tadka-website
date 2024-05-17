5435c1a17ba489c5ad6a76f7f38b23e5e4640a84
KenXD2001 <ashwinb708@gmail.com>
2024-05-17 11:37:54 +0530

    Add Banner Slider

--- src/components/home/data.jsx
+++ src/components/home/data.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+// data.jsx
 
-const Data = () => {
-    return (
-        <div className="home__data">
-            <h1 className="home__title">Ashwin Bhardwaj
-            </h1>
-        </div>
-    )
-}
+import slide1 from '../../assets/banners/slider1.jpg';
+import slide2 from '../../assets/banners/slider2.jpg';
+import slide3 from '../../assets/banners/slider3.jpg';
+import slide4 from '../../assets/banners/slider4.jpg';
+import slide5 from '../../assets/banners/slider5.jpg';
 
-export default Data
+const imageData = {
+    slide1,
+    slide2,
+    slide3,
+    slide4,
+    slide5
+};
+
+const slidesData = [
+    {
+        id: 1, imgKey: 'slide1',
+        heading: 'Quick and Easy Breakfast Recipes for Busy Mornings'
+    },
+    {
+        id: 2, imgKey: 'slide2',
+        heading: 'Healthy and Satisfying Lunch Recipes for Balanced Meals'
+    },
+    {
+        id: 3, imgKey: 'slide3',
+        heading: 'Hearty Dinners the Whole Family Will Love'
+    },
+    {
+
+        id: 4, imgKey: 'slide4',
+        heading: 'Indulgent Chocolate Desserts You Cant Resist Making'
+    },
+    {
+        id: 5, imgKey: 'slide5',
+        heading: 'Festive Recipes for Every Holiday Celebration and Gathering'
+    }
+];
+
+export { imageData, slidesData };
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./home.css";
+// import Social from './social';
+// import Data from './data';
+import ScrollDown from './scrollDown';
+import BannerSlider from './bannerSlider';
 
 
 const Home = () => {
     return (
         <div>
-            <section className="home section" id='home'>
+            <section className="home__section section" id='home'>
+                <BannerSlider />
+                <div className="home__container container grid">
+                    <div className="home__content grid">
+                        {/* <Social /> */}
 
+                        {/* <div className='home__img'></div> */}
+
+                        {/* <Data /> */}
+                    </div>
+
+                    <ScrollDown />
+                </div>
             </section>
         </div>
     )
